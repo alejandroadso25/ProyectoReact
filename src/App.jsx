12bf48209dd450edd407370e7ff65ddce1e03d9e
Alejandro <alejandroadso25@gmail.com>
2026-05-05 14:18:31 -0500

    Rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
-//Importaciones 
+// Importaciones 
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 import Section from "./components/Section";
 import Aside from "./components/Aside";
 import Footer from "./components/Footer";
+
 import "./components/App.css";
 
-//Llamo los componentes
+// App con rutas
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Main />
-      <Section />
-      <Aside />
+
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/section" element={<Section />} />
+        <Route path="/aside" element={<Aside />} />
+      </Routes>
+
       <Footer />
-    </>
+    </Router>
   );
 }
+
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,21 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
     <nav className="navbar">
       <h2>Mi Proyecto React</h2>
+
+      <ul>
+        <li>
+          <Link to="/">Inicio</Link>
+        </li>
+        <li>
+          <Link to="/section">Sección</Link>
+        </li>
+        <li>
+          <Link to="/aside">Aside</Link>
+        </li>
+      </ul>
     </nav>
   );
 }
